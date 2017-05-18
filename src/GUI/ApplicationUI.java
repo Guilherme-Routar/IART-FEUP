88d91161dcd9b49ba133953dc54d15ffe74c72d5
Guilherme-Routar <groutar@gmail.com>
2017-05-19 00:56:58 +0100

    Some progress on swing

--- src/GUI/ApplicationUI.java
+++ src/GUI/ApplicationUI.java
@@ -1,23 +1,48 @@
 package GUI;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractButton;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class ApplicationUI extends JFrame {
-    public static void main(String[] args) {
-        try {
-        	ApplicationUI frame = new ApplicationUI();
-            frame.setVisible(true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	/**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add the ubiquitous "Hello World" label.
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
     }
-    public ApplicationUI() {
-        setBounds(100, 100, 450, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    
-    public static void main()
-    {
-    	new ApplicationUI();
+
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame("Online News Popularity Predictor");
+        
+        JButton b1 = new JButton("Disable middle button");
+        b1.setVerticalTextPosition(AbstractButton.CENTER);
+        b1.setHorizontalTextPosition(AbstractButton.LEADING); //aka LEFT, for left-to-right locales
+        b1.setMnemonic(KeyEvent.VK_D);
+        b1.setActionCommand("disable");
+
+        //ImageIcon loading = new ImageIcon("ajax.gif");
+        //frame.add(new JLabel("loading... ", loading, JLabel.CENTER));
+        frame.add(b1);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setVisible(true);
     }
 }
 
